d8d0908bd76d169653fb2f91c13c3f30f2e4c8dd
weihua <175990271@qq.com>
2024-05-19 17:35:39 +0800

    999

--- app/root.tsx
+++ app/root.tsx
@@ -11,8 +11,8 @@ import {
 import { useContext, useEffect, useRef } from "react";
 
 import "@fontsource/montserrat/300.css";
-import "@fontsource/montserrat/500.css";
-import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/600.css";
 
 import { CssBaseline, Experimental_CssVarsProvider } from "@mui/material";
 import ClientStyleContext from "~/styles/client.context";
